5a9447092ee0eafac4df944dc4426849bbaa02cd
Jungminchae <minchae3618@gmail.com>
2020-10-24 23:54:03 +0900

    login template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  purge: [],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "h-25vh":"25vh",
+        "h-50vh":"50vh",
+        "h-75vh":"75vh",
+      },
+      borderRadius: {
+        xl: "1.5rem"
+      }
+    },
   },
   variants: {},
   plugins: [],
